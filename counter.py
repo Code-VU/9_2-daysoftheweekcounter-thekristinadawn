f98e334a01f1f206baacbd5766ae0a576efff21a
Kristina Palmer <Kristina.Palmer@vu.com>
2022-03-27 13:11:22 -0500

    1st Attempt

--- counter.py
+++ counter.py
@@ -1,10 +1,32 @@
 
 def countDayOfTheWeek():
     # This first line is provided for you
-    file_name = input("Enter a file name: ")    # end assignment
+    file_name = input("Enter a file name: ")
+    days_dictionary = dict()
 
+    try: 
+        file_open = open(file_name)
+    except: 
+        print('File cannot be opened:', file_name)
+        exit()
+
+    for line in file_open: 
+        words = line.split() 
+        #print(line)
+        if line.startswith('From '): 
+            if words[2] not in days_dictionary: 
+                days_dictionary[words[2]] = 1
+            else: 
+                days_dictionary[words[2]] += 1 
+    print(days_dictionary)
+
+        
+
+countDayOfTheWeek()
+
+# end assignment
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-##countDayOfTheWeek()
+#countDayOfTheWeek()
